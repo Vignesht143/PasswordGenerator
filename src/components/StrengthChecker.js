91964d4cd43a59d6c14ff4278a486d46341e9937
Vignesht143 <vigneshkvg22@gmail.com>
2023-11-20 22:18:34 +0530

    Initial commit

---
+++ src/components/StrengthChecker.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const PasswordStrengthIndicator =({password=""})=>{
+  const getPasswordStrength=()=>{
+    const passwordLength =password.length;
+    if(passwordLength<1){
+      return ""
+    }else if (passwordLength<4){
+      return "very weak"
+    }else if (passwordLength<8){
+      return "Poor"
+    }else if (passwordLength<12){
+      return "Medium"
+    }else if (passwordLength<16){
+      return "Strong"
+    }else if (passwordLength<20){
+      return "Very strong"
+    }
+  };
+  const passwordStrength =getPasswordStrength()
+  if(!passwordStrength)
+  return <React.Fragment />
+  return(
+    <div className="password-strength">Strength:<span style={{fontWeight:"bold"}}>{passwordStrength}</span></div>
+  )
+};
+export default PasswordStrengthIndicator;
